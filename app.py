3bf3085dd2e085db33729f8605dc838c9b5708a0
Kelly Diabagate <diabagatekelly@yahoo.fr>
2021-03-29 00:15:14 -0500

    session

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "kelly-af-01221990")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///tajweed')
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('REDIS_URL'))
+# app.config['SESSION_TYPE'] = 'redis'
+# app.config['SESSION_REDIS'] = redis.from_url(os.environ.get('REDIS_URL'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -37,8 +37,8 @@ connect_db(app)
 
 
 # db = SQLAlchemy()
-sess = Session()
-sess.init_app(app)
+# sess = Session()
+# sess.init_app(app)
 
 CORS(app, supports_credentials=True)
 session_cookie = SecureCookieSessionInterface().get_signing_serializer(app)
